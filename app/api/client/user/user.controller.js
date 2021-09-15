ffee3dd927ab610ae159faa7b89af0189bdb924b
thuan2172001 <thuan2172001@gmail.com>
2021-09-15 16:57:55 +0700

    fix: cart

--- app/api/client/user/user.controller.js
+++ app/api/client/user/user.controller.js
@@ -4,9 +4,20 @@ import CommonError from '../../library/error';
 import {
     success,
 } from '../../../utils/response-utils';
-import { getCart, updateCart } from "./user.service";
+import { getCart, updateCart, getCartData } from "./user.service";
 
 const api = express.Router();
+
+api.get('/user/cart-data', CheckAuth, async (req, res) => {
+    try {
+        const userId = req.userInfo && req.userInfo._id ? req.userInfo._id : '';
+        const results = await getCartData({ userId });
+
+        return res.json(success(results));
+    } catch (err) {
+        return CommonError(req, err, res);
+    }
+});
 
 api.get('/user/cart', CheckAuth, async (req, res) => {
     try {
@@ -23,7 +34,9 @@ api.put('/user/cart', CheckAuth, async (req, res) => {
     try {
         const userId = req.userInfo && req.userInfo._id ? req.userInfo._id : '';
         const { cartItems } = req.body
-        const results = await updateCart({ userId, cartItems });
+        const statusCode = await updateCart({ userId, cartItems });
+
+        const results = statusCode > 0 ? 'success' : 'failed';
 
         return res.json(success(results));
     } catch (err) {
--- app/api/client/user/user.service.js
+++ app/api/client/user/user.service.js
@@ -5,7 +5,7 @@ import { Episodes } from "../../../models/episode";
 const { DataTypes } = require('sequelize');
 const uuidv1 = require('uuidv1');
 
-export const getCart = async ({ userId }) => {
+export const getCartData = async ({ userId }) => {
     const user = await Users.findOne({ where: { _id: userId } });
 
     if (!user) throw new Error('USER.USER_NOT_FOUND');
@@ -22,6 +22,18 @@ export const getCart = async ({ userId }) => {
     }))
 
     return result;
+}
+
+export const getCart = async ({ userId }) => {
+    const user = await Users.findOne({ where: { _id: userId } });
+
+    if (!user) throw new Error('USER.USER_NOT_FOUND');
+
+    const cart = await Carts.findOne({ where: { userId } });
+
+    const episodesId = cart.dataValues?.cartItems || [];
+
+    return episodesId;
 }
 
 export const updateCart = async ({ userId, cartItems }) => {
