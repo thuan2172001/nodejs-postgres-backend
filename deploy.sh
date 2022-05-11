efd8cfcd0a92a8158080634be615c0977edfc896
thuan2172001 <thuan2172001@gmail.com>
2022-05-12 00:17:15 +0800

    fix: sh

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+nohup http-server -p 3002
+nohup ssh -o ServerAliveInterval=20 -p 22222 -R webtoonz:80:localhost:4200 uetbc.xyz
+nohup ssh -o ServerAliveInterval=20 -p 22222 -R webtoonz-s3:80:localhost:3002 uetbc.xyz
